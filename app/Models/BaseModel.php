e7ec2ecb9413495fe85a598c3b1f4a9d512fd21b
darighteous1 <tonislav.atanasov@gmail.com>
2024-12-30 23:33:14 +0200

    introduce BaseModel.php

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    use HasUuids;
+}
