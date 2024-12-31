d03e156bedcb361dce6d39bb33bf2164e28d5714
darighteous1 <tonislav.atanasov@gmail.com>
2024-12-31 14:06:41 +0200

    remove HasUuids trait from User class

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -15,7 +16,6 @@ class User extends Authenticatable
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
-    use HasUuids;
 
     /**
      * The attributes that are mass assignable.
